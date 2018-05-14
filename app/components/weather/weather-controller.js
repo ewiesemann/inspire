cda7f591d914cf68ed4b5b56118d2dc6ddef4a05
Name <username@email.com>
2018-05-14 14:17:50 -0600

    Added background to items for visibility and added in temp icon

--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -1,12 +1,15 @@
-function WeatherController(){
+function WeatherController() {
 	var wc = this;
 	var weatherService = new WeatherService();
-	
-	weatherService.getWeather(function(weather){
+
+	weatherService.getWeather(function (weather) {
 		console.log(weather);
-		var tempF = Math.floor((9/5)*(weather.main.temp)-459.67)
+		console.log(weather.weather[0].icon)
+		var tempF = Math.floor((9 / 5) * (weather.main.temp) - 459.67)
+		var img = "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png"
 		var template = `
-		<h1>Current Temp: ${tempF}°</h1>
+		<h1>Current Temp: ${tempF}°<img src="${img}"></h1>
+		
 		<h2>City: ${weather.name}</h2>
 		<h3>Humidity: ${weather.main.humidity}<h3>
 		<h3>WindSpeed: ${weather.wind.speed}</h3>
@@ -15,5 +18,5 @@ function WeatherController(){
 		document.getElementById("weather").innerHTML = template
 		//What can you do with this weather object?
 	})
-	
+
 }
